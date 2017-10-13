8887db902356625cd0c1f386011876ec5b0dbfec
cearstein <cearstein@gmail.com>
2017-10-13 10:08:33 -0600

    5, movie quote
    ANOTHER MESSAGE FOR TESSSSSTTTT INGGGGG

--- 09_GitTester/src/GitTest.java
+++ 09_GitTester/src/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
 		System.out.println("A line to demonstrate change");
 		System.out.println("More lines to prove I know what I am doing");
 		System.out.println("ANOTHER ONE");
+		System.out.println("I wanna know, you dont wanna know, I WANNO KNOW, you dont wanna know, I WANNNNAAA KNOWWW. WELL ILL TELL YA");
 
 	}
 
